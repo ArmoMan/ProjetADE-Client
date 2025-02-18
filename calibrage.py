ff46166a383d95dace030d4f938db2c1a10ce9c9
ArmoMan <alaverdyanhayk04@gmail.com>
2025-02-18 14:24:09 -0500

    Ajout de commentaires dans calibrage.py

--- calibrage.py
+++ calibrage.py
@@ -3,11 +3,21 @@ from calibrer.calibrer_ph import CalibrerPh
 from controlleurs.composants.adc.ads1115_controleur import ADS1115Controleur
 from controlleurs.composants.adc.pin_analogique import PinAnalogique
 
-# exemple pour calibrer le ph
-adc = ADS1115Controleur()
-acide = 4.01
-basique = 6.86
-calibrage = CalibrerPh(adc, PinAnalogique.A1, 10, acide, basique)
-#calibrage.commencer()
+#Cette page permet d’effectuer les calibrages avant de lancer le programme.
+
+################################################################
+############### exemple pour calibrer le ph ####################
+################################################################
+acide = 4.01 # valeur de votre ph
+basique = 6.86 # valeur de votre ph
+adc = ADS1115Controleur() #le controleur du ADC
+pinPh =  PinAnalogique.A1 # pin dont votre ph capteur est branché (sur le ADC)
+calibrage = CalibrerPh(adc, pinPh, 10, acide, basique) # initier class calibrage
+
+#calibrage.commencer() # commencer à calibrer
+
 #calibrage.tester_marge(-0.1713915470494417)
-calibrage.tester_marge(-0.1716754385964914)
+calibrage.tester_marge(-0.1716754385964914) # lire une mesure avec déja la marge
+################################################################
+################################################################
+################################################################
